f14e1c4f1f265a433ba988cb1f9d2c8739fe4abd
IvanTyukin <ivantyukin@mail.ru>
2022-12-14 16:17:20 +0300

    Update lab2.java

--- lab2.java
+++ lab2.java
@@ -22,31 +22,28 @@ public class Main {
         int testArray[][] = new int[n][4];
 
         for (int k = 8; k > 0; k--) {
+            int m = Integer.parseInt("11111111",2) - Integer.parseInt(Integer.toBinaryString(255 >> k),2);
             for (int i = 0 ; i < n ; i++) { // k -количество разрядов
-                int m = Integer.parseInt("11111111",2) - Integer.parseInt(Integer.toBinaryString(255 >> k),2);
-                testArray[i][4] = m & ipArray[i][4];
-                int p = 1;
-                for (int t = 0 ; t < n ; t++) {
-                    System.out.print("Подсеть" + p + ": " + ipArray[0][0] + "." + ipArray[0][1] + "." + ipArray[0][2] + "." + m + " Маска: " +
-                                        255 + "." + 255 + "." + 255 + "." + testArray[i][4] + " ");
-                    for (int f = t+1 ; f < n ; f++) {
-
+                testArray[i][3] = m & ipArray[i][3];
+            }
+            
+            int p = 1;
+            for (int t = 0 ; t < n ; t++) {
+                
+                for (int f = t+1 ; f < n ; f++) {
+                    if (testArray[t][3] == testArray[f][3]) {
+                        System.out.print("Подсеть" + p + ": " + ipArray[0][0] + "." + ipArray[0][1] + "." + ipArray[0][2] + "." + testArray[t][3] + " Маска: " +
+                                    255 + "." + 255 + "." + 255 + "." + m + " входят адреса ");
+                        System.out.print(ipArray[t][0] + "." + ipArray[t][1] + "." + ipArray[t][2] + "." + ipArray[t][3] + "," +
+                                        ipArray[f][0] + "." + ipArray[f][1] + "." + ipArray[f][2] + "." + ipArray[f][3]);
                     }
-                    p += 1;
                 }
-
+                p += 1;
             }
-
 
         }
 
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(ipArray[i][j]);
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
+        
     }
 }
